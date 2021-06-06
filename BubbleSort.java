1ef9d05a19555dfd9a13762c654e245d84f6f2dd
NskBeingToxic <85427255+NskBeingToxic@users.noreply.github.com>
2021-06-06 11:14:27 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,9 +4,7 @@ class BubbleSort {
         int n=a.length;
         for(int i=0;i<n-1;i++){
             boolean flag=false;
-            System.out.println((i+1)+" pass");
             for(int j=0;j<n-i-1;j++){
-                System.out.println(a[j]+"--->"+a[j+1]);
                 if(a[j]>a[j+1]){
                     int temp=a[j];
                     a[j]=a[j+1];
